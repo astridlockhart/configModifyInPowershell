73374970a654da1f8a5ea8074e5e1a31fb1928f6
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2026-01-08 10:39:00 -0600

    Update with input error message

--- configModify.ps1
+++ configModify.ps1
@@ -1,5 +1,5 @@
   #in line parameters format: configModify.ps1 <config file> <configuration> <new setting>
-  # v1.1
+  # v1.1.1
 
   # Might wanna take a look at this later [ ('a color: green' -split ':')[0].trim() -match '^a color$' ] this will help
   # with potential trimming of a string!
@@ -15,15 +15,23 @@ param(
     [Parameter()]
     [string]$modifacation = "default" # what we want the new config to be.
     )
-$spacer = " "
-#The purpose of this script it to seek out a point in the config file such as "color:" and change it to what is stored in the $modifaction parameter.
+
+$i = 0
+#The purpose of this script it to seek out a point in the config file such as "color:" and change it to what is stored in the $modifacation parameter.
 foreach($line in Get-Content $sourceFile) { #iterates through txt file.
     if(($line -split ':')[0].trim() -match "^$configEdit$"){ #checks for matching text in current line
         
         $editFile = Get-Content $sourceFile #sets config file for editing
-        $editFile.Replace($line.Substring($configEdit.Length + 1), $spacer + $modifacation) | Set-Content -Path $sourceFile # Edits the line!        
+        $editFile.Replace($line.Substring($configEdit.Length + 1), " " + $modifacation) | Set-Content -Path $sourceFile # Edits the line!   
+        $i++     
     }
 }
 
-Write-Host "-------------[New Config File]-----------------------"
+if($i -eq 0){
+        Write-Host "#########################################"
+        Write-Host "Please check the spelling of the setting!"
+        Write-Host "#########################################"
+    }
+
+Write-Host "-------------[Config File after script execution]-----------------------"
 cat $sourceFile #confirms new config
